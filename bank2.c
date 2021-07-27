29ad3bc2393fdbbaf6e636e77d218c8158a1ab9c
Neos_codes <alexced96@outlook.es>
2021-07-27 12:01:39 -0400

    Hay que rehacer las funciones para esquivar y volver al centro, las anteriores estaban buggeadas

--- bank2.c
+++ bank2.c
@@ -40,9 +40,8 @@ extern void Enemy_anim_idle();
 //============================
 //-----Variables Internas-----
 //============================
-// Posicion de la celda en que se encuentra el slime (0 centro, 1 arriba, 2 abajo, 3 atrás)
-UINT8 slime_movement_frames;
-UINT8 avoiding;
+
+
 
 //============================
 //---- Funciones internas ----
@@ -92,14 +91,12 @@ void IniBattle(){
     // Inicializar parametros de movimiento
     
     slime_dir = 0;      // Slime está en la casilla central
-    slime_movement_frames = 0;
 
     // Inicializar parametros para animaciones
 
     frames_anim = 0;
     state = FALSE;
     isMoving = FALSE;
-    avoiding = FALSE;
 }
 
 void BattleLoop(){
@@ -107,7 +104,7 @@ void BattleLoop(){
     set_bkg_tiles(0, 0, 20, 18, batteMap_test);
 
     IniBattle();
-
+    
     while(1){
 
         if(!vbl_count)
@@ -117,111 +114,7 @@ void BattleLoop(){
         Enemy_anim_idle();
         Slime_anim_idle();
 
-        // Recibir botones
-        input = joypad();
-
-        // Tomar movimientos de Slime
-        if(!isMoving){
-            // Escoger dirección del Slime
-            Take_Input();
-        }
-
-        // Mover Slime
-        if(isMoving){
-            // Sacar Slime del centro
-            if(avoiding){
-                Avoid_Slime();
-            }
-            // Devolver Slime al centro
-            else{
-                Return_toCenter();   // BUG AQUI?
-            }
-        }
-
         frames_anim++;
+
     } // ENDWHILE
-}
-
-void Take_Input(){
-    // Hacia arriba
-    if(input & J_UP){
-        isMoving = TRUE;
-        avoiding = TRUE;
-        slime_dir = 1;
-    } // Hacia abajo
-    else if(input & J_DOWN){
-        isMoving = TRUE;
-        avoiding = TRUE;
-        slime_dir = 2;
-    } // Hacia atrás
-    else if(input & J_LEFT){
-        isMoving = TRUE;
-        avoiding = TRUE;
-        slime_dir = 3;
-    }
-}
-
-void Avoid_Slime(){
-    // Hacia arriba
-    if(slime_dir == 1){
-        scroll_sprite(0, 0, -1);
-        scroll_sprite(1, 0, -1);
-        scroll_sprite(2, 0, -1);
-        scroll_sprite(3, 0, -1);
-    }
-    else if(slime_dir == 2){
-        // Hacia abajo
-        scroll_sprite(0, 0, 1);
-        scroll_sprite(1, 0, 1);
-        scroll_sprite(2, 0, 1);
-        scroll_sprite(3, 0, 1);
-    }
-    else if(slime_dir == 3){
-        // Hacia la izquierda
-        scroll_sprite(0, -1, 0);
-        scroll_sprite(1, -1, 0);
-        scroll_sprite(2, -1, 0);
-        scroll_sprite(3, -1, 0);
-    }
-    // Aumentar contador de pixeles movidos
-    slime_movement_frames++;
-
-    // Si se movió 32 pixeles, vuelve a 0 y a su posicion central
-    if(slime_movement_frames == 32){
-        slime_movement_frames = 0;
-        avoiding = FALSE;
-    }
-}
-
-void Return_toCenter(){
-    // Si esta arriba, volver hacia abajo
-    if(slime_dir == 1){
-        scroll_sprite(0, 0, 1);
-        scroll_sprite(1, 0, 1);
-        scroll_sprite(2, 0, 1);
-        scroll_sprite(3, 0, 1);
-    }
-    else if(slime_dir == 2){
-        // Si esta abajo, volver hacia arriba
-        scroll_sprite(0, 0, -1);
-        scroll_sprite(1, 0, -1);
-        scroll_sprite(2, 0, -1);
-        scroll_sprite(3, 0, -1);
-    }
-    else if(slime_dir == 3){
-        // Si esta a la izquierda, volver hacia el centro
-        scroll_sprite(0, 1, 0);
-        scroll_sprite(1, 1, 0);
-        scroll_sprite(2, 1, 0);
-        scroll_sprite(3, 1, 0);
-    }
-    // Aumentar contador de pixeles movidos
-    slime_movement_frames++;
-
-    // Si se movió 32 pixeles, vuelve a 0 y a su posicion central
-    if(slime_movement_frames == 32){
-        slime_movement_frames = 0;
-        slime_dir = 0;
-        isMoving = FALSE;
-    }
 }
